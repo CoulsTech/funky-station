102bf6dc000088799e1654d765658514cf73ef70
CoulsTech <redaclouls@gmail.com>
2026-03-29 01:03:48 -0400

    Initial commit, where I introduce the skeleton for the component, system, and prototype for a new item. as well as my modifications to the existing system and prototype for the welder to testbed my new system and component.
    My brain is mush...

---
+++ Content.Shared/_Funkystation/Tools/Components/DoorCutterComponent.cs
@@ -0,0 +1,7 @@
+namespace Content.Shared._Funkystation.Tools.Components;
+
+public sealed class DoorCutterComponent
+{
+    public float CutTime = 3.0f;
+    public string ValidTargetTag = "CuttableDoor";
+}
